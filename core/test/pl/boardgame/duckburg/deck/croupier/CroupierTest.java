e42ba17b35769e7d8ba35c1699fea8caa5a890cd
Sarseth <elune90@gmail.com>
2016-12-24 15:53:31 +0100

    Completed tests for random card generator as for current croupier stage

--- core/test/pl/boardgame/duckburg/deck/croupier/CroupierTest.java
+++ core/test/pl/boardgame/duckburg/deck/croupier/CroupierTest.java
@@ -4,6 +4,9 @@ import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -12,6 +15,7 @@ import pl.boardgame.duckburg.deck.GameCardDeck;
 import pl.boardgame.duckburg.deck.cards.Card;
 import pl.boardgame.duckburg.deck.cards.types.CardType;
 
+//TODO: make it parametrized
 public class CroupierTest {
 
 	@BeforeClass
@@ -21,7 +25,7 @@ public class CroupierTest {
 
 	// bit naive, I know.
 	@Test
-	public void checkIfAtLeastOneTownhallCardGenerates() {
+	public void checkIfAtLeastThreeTownhallCardGeneratesForDeck() {
 		Croupier instance = Croupier.getInstance();
 		for(int i = 0; i < 10; i++) {
 			GameCardDeck cardDeck = instance.createRandomDeckOfCards(GameOptions.DeckSize.SMALL);
@@ -35,6 +39,57 @@ public class CroupierTest {
 				}
 			}
 			assertThat(townhallCardCounter >= 3, is(true)); // TODO: No internet. Find greaterThanOrEqualTo matcher and inject into project
+		}
+	}
+
+	@Test
+	public void checkIfAtLeastFourTownhallCardGeneratesForDeck() {
+		Croupier instance = Croupier.getInstance();
+		for(int i = 0; i < 10; i++) {
+			GameCardDeck cardDeck = instance.createRandomDeckOfCards(GameOptions.DeckSize.MEDIUM);
+
+			int townhallCardCounter = 0;
+			for(int cardIterator = 0; cardIterator < GameOptions.DeckSize.MEDIUM.getDeckSize(); cardIterator++) {
+				Card card = cardDeck.pickupCard();
+				assertThat(card, notNullValue());
+				if(CardType.TOWNHALL == card.getCardType()) {
+					townhallCardCounter++;
+				}
+			}
+			assertThat(townhallCardCounter >= 4, is(true)); // TODO: No internet. Find greaterThanOrEqualTo matcher and inject into project
+		}
+	}
+
+	@Test
+	public void checkIfAtLeastFiveTownhallCardGeneratesForDeck() {
+		Croupier instance = Croupier.getInstance();
+		for(int i = 0; i < 10; i++) {
+			GameCardDeck cardDeck = instance.createRandomDeckOfCards(GameOptions.DeckSize.LARGE);
+
+			int townhallCardCounter = 0;
+			for(int cardIterator = 0; cardIterator < GameOptions.DeckSize.LARGE.getDeckSize(); cardIterator++) {
+				Card card = cardDeck.pickupCard();
+				assertThat(card, notNullValue());
+				if(CardType.TOWNHALL == card.getCardType()) {
+					townhallCardCounter++;
+				}
+			}
+			assertThat(townhallCardCounter >= 5, is(true)); // TODO: No internet. Find greaterThanOrEqualTo matcher and inject into project
+		}
+	}
+
+
+	@Test
+	public void noDuplicatesShouldBeGenerated() {
+		Croupier instance = Croupier.getInstance();
+		for(int i = 0; i < 20; i++) {
+			Set<Integer> pickedUpCardsIdSet = new HashSet<>();
+			GameCardDeck cardDeck = instance.createRandomDeckOfCards(GameOptions.DeckSize.LARGE);
+
+			for(int cardIterator = 0; cardIterator < GameOptions.DeckSize.LARGE.getDeckSize(); cardIterator++) {
+				Card card = cardDeck.pickupCard();
+				assertThat(pickedUpCardsIdSet.add(card.getId()), is(true));
+			}
 		}
 	}
 
